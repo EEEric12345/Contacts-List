5df6d6ecc072e87effe95526eceb79dc1218d5a4
EEEric <yzhang023@students.fairmontschools.com>
2023-11-07 10:18:27 -0800

    Update ContactList.java

--- src/main/java/ContactList.java
+++ src/main/java/ContactList.java
@@ -26,6 +26,60 @@ class ContactList {
         }
     }
 
+    // Search for a specific string in a contact's information
+    public void searchContact(String toSearch) {
+        System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s", "Prefix", "First Name", "Middle Name", "Last Names", "Suffix", "Nickname");
+        for (Contact contact : contacts) {
+            String namePrefix = contact.getNamePrefix();
+            String firstName = contact.getFirstName();
+            String middleName = contact.getMiddleName();
+            String lastName = contact.getLastName();
+            String nameSuffix = contact.getNameSuffix();
+            String nickname = contact.getNickname();
+            List<String> phoneNumbers = contact.getPhoneNumbers();
+            List<String> emails = contact.getEmails();
+            List<String> groups = contact.getGroups();
+            List<String> addresses = contact.getAddresses();
+            List<String> importantDates = contact.getimportantDates();
+            String relationship = contact.getRelationship();
+            String notes = contact.getNotes();
+            if (namePrefix.contains(toSearch) || firstName.contains(toSearch) || middleName.contains(toSearch) || lastName.contains(toSearch) || namePrefix.contains(toSearch) || nameSuffix.contains(toSearch) || nickname.contains(toSearch) || relationship.contains(toSearch) || notes.contains(toSearch)) {
+                System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s\nPhone Numbers: %s\nEmails: %s\nGroups: %s\nAdresses: %s\nImportant Dates: %s\nRelationship: %-25s Notes: ", namePrefix, firstName, middleName, lastName, nameSuffix, nickname, phoneNumbers, emails, groups, addresses, importantDates, relationship, notes);
+            } else {
+                for (String phoneNumber : phoneNumbers) {
+                    if (phoneNumber.contains(toSearch)) {
+                        System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s\nPhone Numbers: %s\nEmails: %s\nGroups: %s\nAdresses: %s\nImportant Dates: %s\nRelationship: %-20s Notes: ", namePrefix, firstName, middleName, lastName, nameSuffix, nickname, phoneNumbers, emails, groups, addresses, importantDates, relationship, notes);
+                        break;
+                    }
+                }
+                for (String email : emails) {
+                    if (email.contains(toSearch)) {
+                        System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s\nPhone Numbers: %s\nEmails: %s\nGroups: %s\nAdresses: %s\nImportant Dates: %s\nRelationship: %-20s Notes: ", namePrefix, firstName, middleName, lastName, nameSuffix, nickname, phoneNumbers, emails, groups, addresses, importantDates, relationship, notes);
+                        break;
+                    }
+                }
+                for (String group : groups) {
+                    if (group.contains(toSearch)) {
+                        System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s\nPhone Numbers: %s\nEmails: %s\nGroups: %s\nAdresses: %s\nImportant Dates: %s\nRelationship: %-20s Notes: ", namePrefix, firstName, middleName, lastName, nameSuffix, nickname, phoneNumbers, emails, groups, addresses, importantDates, relationship, notes);
+                        break;
+                    }
+                }
+                for (String address : addresses) {
+                    if (address.contains(toSearch)) {
+                        System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s\nPhone Numbers: %s\nEmails: %s\nGroups: %s\nAdresses: %s\nImportant Dates: %s\nRelationship: %-20s Notes: ", namePrefix, firstName, middleName, lastName, nameSuffix, nickname, phoneNumbers, emails, groups, addresses, importantDates, relationship, notes);
+                        break;
+                    }
+                }
+                for (String importantDate : importantDates) {
+                    if (importantDate.contains(toSearch)) {
+                        System.out.printf("%-10s%-25s%-20s%-25s%-10s%-25s\nPhone Numbers: %s\nEmails: %s\nGroups: %s\nAdresses: %s\nImportant Dates: %s\nRelationship: %-20s Notes: ", namePrefix, firstName, middleName, lastName, nameSuffix, nickname, phoneNumbers, emails, groups, addresses, importantDates, relationship, notes);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     // Save all the contacts to a file
     public void saveContactsToFile(String filename) {
         filename += ".bin";
